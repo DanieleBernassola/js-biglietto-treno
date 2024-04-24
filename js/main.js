8f4d6a55945cb903a968aafbec81a90ec85f2551
DanieleBernassola <danielebernassola02@gmail.com>
2024-04-24 17:23:51 +0200

    [UPD] Added final price for ticket

--- js/main.js
+++ js/main.js
@@ -11,27 +11,25 @@ console.log(age);
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
 
+const prezzoPieno = kilometers * 0.21;
+let prezzoScontato = prezzoPieno;
+
 if(!isNaN(kilometers) && !isNaN(age)) {
-    console.log('chilometri e età sono dei numeri')
+    console.log('chilometri e età sono dei numeri');
+    console.log('Questo è il prezzo iniziale:', prezzoPieno);
+    if(age < 18) {
+        // va applicato uno sconto del 20% per i minorenni
+        prezzoScontato = prezzoPieno - (prezzoPieno * 20 / 100);
+    } else if(age >= 65){
+        // va applicato uno sconto del 40% per gli over 65.
+        prezzoScontato = prezzoPieno - (prezzoPieno * 40 / 100);
+    }
+
 } else {
     console.log('uno dei dati inseriti non è un numero')
 }
 
-//  - definisco una variabile prezzoPieno e salvo la moltiplicazione kilometers * 0.21
-
-// va applicato uno sconto del 20% per i minorenni
-
-//  - definisco una variabile prezzoScontato
- 
-
-// va applicato uno sconto del 40% per gli over 65.
-
-//  - SE age < 18
-//    - prezzoScontato = prezzoPieno - prezzoPieno * 20 / 100 e stampo
-//    ALTRIMENTI SE age > 65
-//      - prezzoScontato = prezzoPieno - prezzoPieno * 40 / 100 e stampo
-//    ALTRIMENTI
-//      - stampo prezzoPieno
+console.log('Questo è il prezzo finale:', prezzoScontato);
 
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 
